cff9a9fbf71eac7a475c41e29b2d3eebc0f31f16
David Durant <dave@dave-desktop.(none)>
2011-06-12 19:05:19 +0100

    Started adding some documentation files

---
+++ Docs/Stand Up Use Cases By Page.mm
@@ -0,0 +1,64 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1307893731166" ID="ID_1141333481" MODIFIED="1307896519874" TEXT="Stand Up Use Cases By Page">
+<node CREATED="1307893851893" ID="ID_1410257363" MODIFIED="1307896505556" POSITION="left" TEXT="Full constituency info">
+<node CREATED="1307893865716" ID="ID_1938218486" MODIFIED="1307893899814">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Multiple ways to rank campaigns
+    </p>
+    <p>
+      effecting constituency
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1307893969484" ID="ID_1242834714" MODIFIED="1307897252859" POSITION="right" TEXT="Splash page (explanation video, skip it cookied in)"/>
+<node CREATED="1307894214906" ID="ID_354547266" MODIFIED="1307894228525" POSITION="left" TEXT="List campaigns by organisation"/>
+<node CREATED="1307894230481" ID="ID_1509969988" MODIFIED="1307894233452" POSITION="right" TEXT="Search">
+<node CREATED="1307894241896" ID="ID_1676256726" MODIFIED="1307894260858" TEXT="Campaign organisations"/>
+<node CREATED="1307894261638" ID="ID_1718696628" MODIFIED="1307894268554" TEXT="Campaign name"/>
+</node>
+<node CREATED="1307894319274" ID="ID_1704755402" MODIFIED="1307898319338" POSITION="right" TEXT="Full Campign Info">
+<node CREATED="1307894327082" ID="ID_165515736" MODIFIED="1307894334997" TEXT="Vote button if able"/>
+<node CREATED="1307894343577" ID="ID_1424180042" MODIFIED="1307898033088" TEXT="Show page stats and edit info if CO"/>
+<node CREATED="1307894358223" ID="ID_1200842885" MODIFIED="1307894370418" TEXT="Edit comment if MP"/>
+<node CREATED="1307896463091" ID="ID_1468885810" MODIFIED="1307896469823" TEXT="Show comments of user&apos;s MP"/>
+<node CREATED="1307898334965" ID="ID_1738852961" MODIFIED="1307898372665">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      If national show info (inc
+    </p>
+    <p>
+      highest votes) for all constituencies
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1307896199695" ID="ID_827945526" MODIFIED="1307896208762" POSITION="left" TEXT="Error Page"/>
+<node CREATED="1307896232732" ID="ID_425635968" MODIFIED="1307897102298" POSITION="left" TEXT="User&apos;s Page">
+<node CREATED="1307896238684" ID="ID_511362063" MODIFIED="1307896253718" TEXT="List of current votes"/>
+<node CREATED="1307896255507" ID="ID_171150614" MODIFIED="1307896277149" TEXT="Consistuency sub-set info"/>
+<node CREATED="1307896335061" ID="ID_1524203751" MODIFIED="1307896341993" TEXT="Validate account"/>
+<node CREATED="1307896348284" ID="ID_1021003979" MODIFIED="1307896353367" TEXT="Leave (remove all info)"/>
+<node CREATED="1307897113122" ID="ID_1752052566" MODIFIED="1307897173833" TEXT="My campaigns (if CO)"/>
+<node CREATED="1307897177613" ID="ID_896126167" MODIFIED="1307897182088" TEXT="My comments (if MP)"/>
+<node CREATED="1307897183549" ID="ID_1233155579" MODIFIED="1307897196807" TEXT="Link to Admin pages (if admin)"/>
+</node>
+<node CREATED="1307896521207" ID="ID_798417364" MODIFIED="1307896532317" POSITION="right" TEXT="Handle users that aren&apos;t logged in">
+<cloud/>
+</node>
+</node>
+</map>
